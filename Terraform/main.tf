c4a78f1082bdba28edf63a56308d0fcdff746ff0
Rachana J S <103162318+Rachana-js@users.noreply.github.com>
2024-12-01 16:43:25 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "instance-1" {
   ami                         = "ami-0453ec754f44f9a4a"
   instance_type               = "t2.micro"
   security_groups             = ["default"]
-  key_name                    = "mumpvt"
+  key_name                    = "project"
   associate_public_ip_address = true
   user_data                   = file("grafana.sh")
   tags = {
@@ -20,7 +20,7 @@ resource "aws_instance" "instance-2" {
   ami                         = "ami-0453ec754f44f9a4a"
   instance_type               = "t2.micro"
   security_groups             = ["default"]
-  key_name                    = "mumpvt"
+  key_name                    = "project"
   associate_public_ip_address = true
   tags = {
     Name = "Node-expo"
