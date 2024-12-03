745a937786f87289ceddfceab2b8dfec1f3a0493
Rachana j s <rachanajadenky@gmail.com>
2024-12-03 17:41:42 +0530

    updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "instance-1" {
   ami                         = "ami-0614680123427b75e"
   instance_type               = "t2.micro"
-  security_groups             = [default]
+  security_groups             = ["default"]
   key_name                    = "mumpvt"
   associate_public_ip_address = true
   user_data                   = file("grafana.sh")
@@ -19,7 +19,7 @@ resource "aws_instance" "instance-1" {
 resource "aws_instance" "instance-2" {
   ami                         = "ami-0614680123427b75e"
   instance_type               = "t2.micro"
-  security_groups             = [default]
+  security_groups             = ["default"]
   key_name                    = "mumpvt"
   associate_public_ip_address = true
   tags = {
